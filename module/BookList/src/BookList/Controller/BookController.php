d80448e104f5cc81ed747ef01861708a0d086536
Jose Checo <checo1983@gmail.com>
2015-12-08 14:43:31 +0100

    First commit BookList Zend2 project

---
+++ module/BookList/src/BookList/Controller/BookController.php
@@ -0,0 +1,26 @@
+<?php 
+namespace BookList\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class BookController extends AbstractActionController {
+	
+	public function indexAction() {
+		return new ViewModel(array(
+			'books' => []
+		));
+	}
+
+	public function addAction() {
+		
+	}
+
+	public function editAction() {
+		
+	}
+
+	public function deleteAction() {
+		
+	}
+}
